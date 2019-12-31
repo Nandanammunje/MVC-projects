7f8809aca5cab9c41bdab30691de933b486f53fc
Nandan Nayak <nandannayak15@gmail.com>
2020-01-01 02:15:06 +0530

    pathbug fixed

--- restdata/src/main/java/com/restdata/config/BackendConstants.java
+++ restdata/src/main/java/com/restdata/config/BackendConstants.java
@@ -3,5 +3,5 @@ package com.restdata.config;
 public class BackendConstants {
 
 	
-	public static final String DOCPATH="/Users/nandannayak/Documents/Gate-ECE";
+	public static final String DOCPATH="/Gate-ECE";
 }
