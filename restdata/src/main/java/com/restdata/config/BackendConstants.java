c073d00a41e86c2c6cbe9585b44d63fd4acf8669
Nandan Nayak <nandannayak15@gmail.com>
2020-01-01 02:01:28 +0530

    restdatapublisherbugfixes

---
+++ restdata/src/main/java/com/restdata/config/BackendConstants.java
@@ -0,0 +1,7 @@
+package com.restdata.config;
+
+public class BackendConstants {
+
+	
+	public static final String DOCPATH="/Users/nandannayak/Documents/Gate-ECE";
+}
--- restdata/src/main/java/com/restdata/dao/FilecontentDaoImpl.java
+++ restdata/src/main/java/com/restdata/dao/FilecontentDaoImpl.java
@@ -3,6 +3,8 @@ import java.io.File;
 
 import org.springframework.stereotype.Repository;
 
+import com.restdata.config.BackendConstants;
+
 @Repository
 class FilecontentDaoImpl implements FilecontentDao {
 
@@ -10,7 +12,7 @@ class FilecontentDaoImpl implements FilecontentDao {
 	public String[] filecontent() {
 		// TODO Auto-generated method stub
 		
-		File folder=new File("C:/Users/nandannayak/Documents/Gate-ECE");
+		File folder=new File(BackendConstants.DOCPATH);
 		 String filelist[]=folder.list();
 		  return filelist;
 	}
