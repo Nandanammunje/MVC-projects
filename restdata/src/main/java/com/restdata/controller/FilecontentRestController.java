0beec7be7eba020e7d3f47c7d8b6470c4a05526f
Nandan Nayak <nandannayak15@gmail.com>
2020-01-01 14:53:46 +0530

    Getmapping to postmapping

--- restdata/src/main/java/com/restdata/controller/FilecontentRestController.java
+++ restdata/src/main/java/com/restdata/controller/FilecontentRestController.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,7 @@ public class FilecontentRestController {
      @Autowired
 	FilecontentService filecontentservice;
 	
-	@GetMapping("/getfilename")
+	@PostMapping("/getfilename")
 	public List<String> FilecontentController()
 	{
 		
