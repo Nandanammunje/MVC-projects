b63df0c743d9db8d263633a6d73ac4895a8beeb6
Nandan Nayak <nandannayak15@gmail.com>
2020-01-12 02:32:47 +0530

    Caching service

--- Archives/src/main/java/com/libra/ftp/Dao/FileDownloadDaoImpl.java
+++ Archives/src/main/java/com/libra/ftp/Dao/FileDownloadDaoImpl.java
@@ -8,6 +8,9 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
+import org.apache.jcs.JCS;
+import org.apache.jcs.access.exception.CacheException;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Repository;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -16,22 +19,39 @@ import com.libra.ftp.Constants.BackendConstants;
 @Repository
 public class FileDownloadDaoImpl implements FileDownloadDao {
 
+	 Logger logger = Logger.getLogger(FileDownloadDaoImpl.class);
 	@Override
 	public String[] GetDocList() {
 		// TODO Auto-generated method stub
 		HttpClient client=HttpClientBuilder.create().build();
 		HttpPost post=new HttpPost(BackendConstants.LIBRAARCHIVELISTURL);
 		String BookList[]=null;
+		JCS cache;
 		try {
+			cache=JCS.getInstance("library");
+		    BookList=(String[])cache.get("Books");
+		    if(BookList!=null && BookList.length!=0)
+		    {
+		    logger.debug("extracted from the cache"+BookList.length);
+		    logger.debug("cace stats"+cache.getStats());
+		    System.out.println("extracted from cache");
+		    }
+		    else
+		    {
+		    	logger.info("cache status before addition"+cache.getStats());
 			HttpResponse response=client.execute(post);
 			HttpEntity entity=response.getEntity();
 			String content=EntityUtils.toString(entity);
 			ObjectMapper mapper=new ObjectMapper();
 			 BookList=mapper.readValue(content,String[].class);
-		
-		} catch (IOException e) {
+			 cache.put("Books",BookList);
+			 logger.info("cache status after addition "+cache.getStats());
+			 
+		    }
+		} catch (IOException | CacheException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			logger.error("Exception occured"+e);
 		}
 		return BookList;
 	}
