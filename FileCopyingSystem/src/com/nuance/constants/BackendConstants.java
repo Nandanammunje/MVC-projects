78feb1b8ff8476e455907432ce69a8b49bc04646
Nandan Nayak <nandannayak15@gmail.com>
2020-01-13 23:38:39 +0530

    Static project deleted

--- FileCopyingSystem/src/com/nuance/constants/BackendConstants.java
+++
@@ -1,9 +0,0 @@
-package com.nuance.constants;
-
-public class BackendConstants {
-
-	
-	
-	public static final String EXTEXCLUDED=".pdf";
-	public static final String FILESOURCE="C:/Users/nandannayak/Documents/Replica";
-}
