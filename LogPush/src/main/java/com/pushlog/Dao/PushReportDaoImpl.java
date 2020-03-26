c82b48c3f49b736ebd07dc7d2ec78d6d39eabd6c
Nandan Nayak <nandannayak15@gmail.com>
2020-03-26 22:52:24 +0530

    updated for download frequency

--- LogPush/src/main/java/com/pushlog/Dao/PushReportDaoImpl.java
+++ LogPush/src/main/java/com/pushlog/Dao/PushReportDaoImpl.java
@@ -1,9 +1,14 @@
 package com.pushlog.Dao;
 
+import java.util.List;
+
+import javax.persistence.Query;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import com.pushlog.entity.DownloadFrequency;
 import com.pushlog.entity.DownloadReport;
 import com.pushlog.entity.UploadReport;
 
@@ -14,9 +19,11 @@ public class PushReportDaoImpl implements PushReportDao{
 		// TODO Auto-generated method stub
 		SessionFactory factory=new Configuration().configure("hibernate.cfg.xml")
 				               .addAnnotatedClass(DownloadReport.class).addAnnotatedClass(UploadReport.class).
+				                addAnnotatedClass(DownloadFrequency.class).
 				               buildSessionFactory();
 		DownloadReport report = null;
 		UploadReport record=null;
+		DownloadFrequency stat=null;
 		Session session=factory.getCurrentSession();
 		try
 		{
@@ -26,6 +33,23 @@ public class PushReportDaoImpl implements PushReportDao{
 	      report.setId(0);
 	      session.beginTransaction();
 		  session.save(report);
+		    Query query=session.createQuery("from DownloadFrequency s where s.name =:name ");
+		    query.setParameter("name",report.getName());
+		      List<DownloadFrequency> statrecord=query.getResultList();
+		    if(statrecord==null||statrecord.size()==0)
+		    {
+		    	stat=new DownloadFrequency();
+		    	stat.setFrequency(1);
+		    	stat.setName(report.getName());
+		    	stat.setId(0);
+		    	session.save(stat);
+		    }
+		    else
+		    {
+		    	stat=statrecord.get(0);
+		    	stat.setFrequency(stat.getFrequency()+1);
+		    	session.saveOrUpdate(stat);
+		    }
 		 
 		}
 		else
